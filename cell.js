d72dfabda405ba1966fd9cb1e0681cf59cda0427
joski2490 <68449917+joski2490@users.noreply.github.com>
2022-12-08 18:44:25 -0600

    Update cell.js

--- cell.js
+++ cell.js
@@ -7,10 +7,10 @@ class Cell {
         this.characteristic = 5 - Math.floor(Math.random()*10);
         
         this.birth = this.environment.time;
-        this.metabolicAge = 0;
-        this.metabolicRate = 1;
-        this.liquid = ['w'];
-        this.solid = [];
+        this.metabolicAge = 1;//0
+        this.metabolicRate = 3;//1
+        this.liquid = ['○'];//w
+        this.solid = [●];//[]
         this.energy = '|||||';
         this.size = 1;
         this.maxCap = 4;
